f3448aaa52ef2e96dbbb3360db0fe1b81662bd4b
demirhanaydin <demirhanaydin@gmail.com>
2017-02-23 10:21:22 +0300

    remove date block from api template for typescript-angular2 & add custom executable script

---
+++ bin/typescript-angular2-portakal.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+SCRIPT="$0"
+
+while [ -h "$SCRIPT" ] ; do
+  ls=`ls -ld "$SCRIPT"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    SCRIPT="$link"
+  else
+    SCRIPT=`dirname "$SCRIPT"`/"$link"
+  fi
+done
+
+if [ ! -d "${APP_DIR}" ]; then
+  APP_DIR=`dirname "$SCRIPT"`/..
+  APP_DIR=`cd "${APP_DIR}"; pwd`
+fi
+
+executable="./modules/swagger-codegen-cli/target/swagger-codegen-cli.jar"
+
+if [ ! -f "$executable" ]
+then
+  mvn clean package
+fi
+
+# if you've executed sbt assembly previously it will use that instead.
+export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
+ags="$@ generate -i http://lvh.me:3000/api/v1/docs.json -l typescript-angular2 -o samples/client/portakal/typescript-angular2/default"
+
+java $JAVA_OPTS -jar $executable $ags
+
+cp -r samples/client/portakal/typescript-angular2/default/ /Users/demirhanaydin/projects/portakalmis/mobile/devdactic-simpleLogin/src/app/api
